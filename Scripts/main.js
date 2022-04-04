f487c88f075df8111edaf9a8f8ee721d3bb2f455
JaneVanilla <janevanilla92@gmail.com>
2022-04-04 16:08:47 +0300

    'testing'

---
+++ Scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".news-block__carousel").owlCarousel({
+        center: true,
+        //rtl: true,
+        loop: true,
+        margin: 30,
+        nav: true,
+        items: 1,
+    })
+});
